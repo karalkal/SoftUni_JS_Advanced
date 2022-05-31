fc722dd4750afab06a7716bde73d00e88eb58936
karalkal <karalkal@yahoo.co.uk>
2022-06-01 00:31:46 +0100

    added solution to exercise 4, task 5

---
+++ 04-dom-intro/b.05. Table - Search Engine/solution.js
@@ -0,0 +1,22 @@
+function solve() {
+   document.querySelector('#searchBtn').addEventListener('click', onClick);
+
+   function onClick() {
+      let searchTerm = document.getElementById('searchField').value
+      let rows = Array.from(document.querySelectorAll('tbody tr'))
+      // console.log(searchTerm)
+
+
+      for (let row of rows) {
+         // console.log(row.textContent)
+
+         if (row.textContent.includes(searchTerm)) {
+            row.setAttribute('class', 'select')
+         } else {
+            row.setAttribute('class', '')  // clear from previous search
+
+         }
+      }
+      document.getElementById('searchField').value = ''
+   }
+}
