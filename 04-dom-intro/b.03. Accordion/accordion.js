d2f5f21bd259537cce6a8b61e7b0032d4352803c
karalkal <karalkal@yahoo.co.uk>
2022-05-31 23:45:49 +0100

    added solutions to exercise 4, tasks 3, 4

--- 04-dom-intro/b.03. Accordion/accordion.js
+++ 04-dom-intro/b.03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    if (document.querySelector('div span.button').textContent === 'More') {
+        document.querySelector('div span.button').textContent = 'Less';
+        document.getElementById('extra').style.display = 'block';
+    }
+    else if (document.querySelector('div span.button').textContent === 'Less') {
+        document.querySelector('div span.button').textContent = 'More';
+        document.getElementById('extra').style.display = 'none';
+    }
 }
+
+// #extra {    display: none;}
--- 04-dom-intro/b.04. Search in List/search.js
+++ 04-dom-intro/b.04. Search in List/search.js
@@ -1,3 +1,14 @@
 function search() {
-   // TODO
+   const cities = Array.from(document.querySelectorAll('#towns li'));
+   let searchTerm = document.getElementById('searchText').value;
+   let counter = 0;
+
+   for (city of cities) {
+      if (city.textContent.includes(searchTerm)) {
+         counter++;
+         city.style['font-weight'] = 'bold';
+         city.style['text-decoration'] = 'underline';
+      }
+   }
+   document.querySelector('div#result').textContent =  `${counter} matches found`
 }
