f2b25384a21cc4edcdf9a5ef1638f1bf8a03830c
karalkal <karalkal@yahoo.co.uk>
2022-06-07 01:22:20 +0100

    added solution to lab 5, tasks 1, 2 + tasks' descriptions

---
+++ 06-advanced-functions/a.02.add-with-context.js
@@ -0,0 +1,16 @@
+function solution(number) {
+
+    function addNumber(externalNumber) {
+        return number + externalNumber
+
+    }
+    return addNumber
+}
+
+let add5 = solution(5);
+console.log(add5(2));
+console.log(add5(3));
+
+let add7 = solution(7);
+console.log(add7(2));
+console.log(add7(3));
